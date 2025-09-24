8660a07d99a3db7277b6eb25f4915789b97c16b6
Omar Gamal <omarthefirst6206@gmail.com>
2025-09-24 17:44:09 +0300

    feat: add SplashView and update styles, colors, and assets

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:bookly_app/core/constants/app_colors.dart';
+import 'package:bookly_app/core/constants/app_styles.dart';
 import 'package:flutter/material.dart';
 
 class SplashViewBody extends StatefulWidget {
@@ -11,9 +13,21 @@ class _SplashViewBodyState extends State<SplashViewBody> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        
-        
+        Image.asset("assets/images/Logo.png",
+        //  color: AppColors.blackColor
+         ),
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: Center(
+            child: Text(
+              "Read Free Books",
+              style: AppStyles.logoText,
+            ),
+          ),
+        ),
       ],
     );
   }
---
+++ lib/core/constants/app_styles.dart
@@ -0,0 +1,37 @@
+import 'package:bookly_app/core/constants/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  static final TextStyle logoText = TextStyle(
+    fontSize: 20,
+    color: AppColors.whiteColor,
+    fontWeight: FontWeight.w400,
+  );
+  // static final TextStyle boldBlackText = TextStyle(
+  //   fontSize: 26,
+  //   fontWeight: FontWeight.bold,
+  // );
+
+  // static final TextStyle w600DarkGrayText = TextStyle(
+  //   fontSize: 18,
+  //   fontWeight: FontWeight.w600,
+  // );
+
+  // static final TextStyle appBarText = TextStyle(
+  //   color: AppColors.blackColor,
+  //   fontSize: 22,
+  //   fontWeight: FontWeight.bold,
+  // );
+
+  // static final TextStyle bodytext = TextStyle(
+  //   color: AppColors.whiteColor,
+  //   fontSize: 20,
+  //   fontWeight: FontWeight.w600,
+  // );
+
+  // static final TextStyle buttonText = TextStyle(
+  //   color: AppColors.whiteColor,
+  //   fontSize: 17,
+  //   fontWeight: FontWeight.w400,
+  // );
+}
