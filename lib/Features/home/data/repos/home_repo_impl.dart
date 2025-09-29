e069a1a533663e83bd7825f79f91c20805ab9ca5
Omar Gamal <omarthefirst6206@gmail.com>
2025-09-29 17:21:07 +0300

    feat: implement fetchFeaturedBooks and fetchNewestBooks methods in HomeRepoImpl; update ApiService for improved API interaction

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -1,16 +1,25 @@
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+  HomeRepoImpl({required this.apiService});
   @override
   Either<Failure, List<BookModel>> fetchFeaturedBooks() {
+    apiService.get(
+      endPoint: "volumes?q=programming",
+    );
     throw UnimplementedError();
   }
 
   @override
   Either<Failure, List<BookModel>> fetchNewestBooks() {
+    apiService.get(
+      endPoint: "volumes?q=programming",
+    );
     throw UnimplementedError();
   }
 }
