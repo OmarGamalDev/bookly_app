9ae48d9c0da2270c445d2dfb6cf7f5b2abe9c04c
Omar Gamal <omarthefirst6206@gmail.com>
2025-10-01 22:14:44 +0300

    feat: implement fetchSimilarBooks method in HomeRepoImpl for retrieving similar books based on category

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -5,4 +5,5 @@ import 'package:dartz/dartz.dart';
 abstract class HomeRepo {
   Future<Either<Failure,List<BookModel>>> fetchNewestBooksImages();
   Future<Either<Failure,List<BookModel>>> fetchNewestBooksDetails();
+  Future<Either<Failure,List<BookModel>>> fetchSimilarBokks({required String category});
 }
--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -47,4 +47,24 @@ class HomeRepoImpl implements HomeRepo {
       return left(ServerFailure(e.toString()));
     }
   }
+  
+@override
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBokks({required String category}) async {
+    try {
+      var data = await apiService.get(
+        endPoint:
+            "volumes?Filtering=free-ebooks&Sorting=relevance&q=subject:$category",
+      );
+      List<BookModel> books = [];
+      for (var item in data["items"]) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
+  }
 }
