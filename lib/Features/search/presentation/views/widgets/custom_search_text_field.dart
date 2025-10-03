8a6a8a7bf7900615ce98d16343362b6a032e852a
Omar Gamal <omarthefirst6206@gmail.com>
2025-10-03 22:46:05 +0300

    feat: integrate SearchCubit into search view; implement search result handling with loading and error states

--- lib/Features/search/presentation/views/widgets/custom_search_text_field.dart
+++ lib/Features/search/presentation/views/widgets/custom_search_text_field.dart
@@ -1,7 +1,9 @@
+import 'package:bookly_app/Features/search/presentation/manager/search_cubit/search_cubit.dart';
 import 'package:bookly_app/core/utils/app_colors.dart';
 import 'package:bookly_app/generated/locale_keys.g.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CustomSearchTextField extends StatelessWidget {
   const CustomSearchTextField({super.key});
@@ -24,6 +26,9 @@ class CustomSearchTextField extends StatelessWidget {
           borderSide: BorderSide(color: Colors.grey.shade300),
         ),
       ),
+      onChanged: (value)  {
+        BlocProvider.of<SearchCubit>(context).fetchSearchResult(value);
+      },
     );
   }
 }
--- lib/Features/search/presentation/views/widgets/search_result_list_view.dart
+++ lib/Features/search/presentation/views/widgets/search_result_list_view.dart
@@ -1,21 +1,30 @@
-import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/best_seller_list_view_item.dart';
+import 'package:bookly_app/Features/search/presentation/manager/search_cubit/search_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SearchResultListView extends StatelessWidget {
   const SearchResultListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      physics: NeverScrollableScrollPhysics(),
-      shrinkWrap: true,
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return BestSellerListViewItem(
-          bookModel: BookModel(
-          ),
-        );
+    return BlocBuilder<SearchCubit, SearchState>(
+      builder: (context, state) {
+        if (state is SearchLoading) {
+          return Center(child: CircularProgressIndicator());
+        } else if (state is SearchFailure) {
+          return Center(child: Text(state.message));
+        } else if (state is SearchSuccess) {
+          return ListView.builder(
+            physics: NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: state.books.length,
+            itemBuilder: (context, index) {
+              return BestSellerListViewItem(bookModel: state.books[index]);
+            },
+          );
+        }
+        return Container();
       },
     );
   }
